da8eade0dfbf8a71e8d6eda7d10d1a97b6a0f8b0
Thomas Henson <thomasjameshenson@gmail.com>
2019-03-28 15:04:38 +0000

    Removed print statements and added write to S3.

--- src/imputation.py
+++ src/imputation.py
@@ -6,74 +6,6 @@ import os
 client = Algorithmia.client()
 
 
-def apply(input):
-
-    # API calls will begin at the apply() method, with the request body passed as 'input'
-    # For more details, see algorithmia.com/developers/algorithm-development/languages
-
-    try:
-        result = do_imputation()
-    except Algorithmia.errors.AlgorithmException as exc:
-        return {
-            "success": False,
-            "error": _get_traceback(exc)
-        }
-    except Exception as exc:
-        return {
-            "success": False,
-            "error": "Unexpected exception {}".format(_get_traceback(exc))
-        }
-
-    output = result.to_csv(index=False)
-
-    # Algorithmia.client().file("data://glanvl/BMIData/output_Lamb_Test.csv").put(str(output))
-    print("Stage: Final Output Sent")
-    return {
-            "success": True,
-            "data": output  # .to_json(orient='records')[1:-1].replace('}, {','},{')
-            }
-
-
-def do_imputation():
-    print("Stage: Running do_imputation")
-    period = 201809
-    cwd = os.path.dirname(__file__)
-    imported_data = pd.read_csv('{}/tests/fixtures/test_data_imputation.csv'.format(cwd), index_col=False)
-    responder_data = check_non_response(imported_data, period)
-    print(str(responder_data))
-
-    return responder_data  # .to_json(orient='records')
-
-
-def check_non_response(data, period):
-    print("Stage: Running check_non_response")
-    print('       Received period: ' + str(period))
-    print('       Received data....')
-    print(str(data))
-    # Create a dataframe where the response column value is set as 1 i.e non responders
-    non_response_filter = (data["response_type"] == 1) & (data["period"] == period)
-    filtered_non_responders = data[non_response_filter]
-    # check the length of the dataset - if there are rows then run imputation...
-    response_check = len(filtered_non_responders.index)
-    if response_check > 0:
-        print('Non Responders identified, calculating imputation factors...')
-        # Ensure that only responder_ids with a response type of 2 (returned) get
-        # picked up
-        data = data[data['response_type'] == 2]
-        # Select and order the columns required for imputation.
-        ordered_data = data[['responder_id', 'land_or_marine', 'region', 'strata', 'Q601_asphalting_sand',
-                             'Q602_building_soft_sand', 'Q603_concreting_sand', 'Q604_bituminous_gravel',
-                             'Q605_concreting_gravel', 'Q606_other_gravel',
-                             'Q607_constructional_fill', 'Q608_total', 'period'
-
-                             ]]
-        return ordered_data
-    else:
-        print('There are no non responders!')
-        print('Skipping Imputation!')
-        return
-
-
 def _get_fh(data_url):
     """
     Opens Algorithmia data urls and returns a file object.
@@ -105,5 +37,64 @@ def _get_traceback(exception):
     )
 
 
-if __name__ == "__main__":
-    apply("z")
+def apply(input):
+
+    # API calls will begin at the apply() method, with the request body passed as 'input'
+    # For more details, see algorithmia.com/developers/algorithm-development/languages
+
+    try:
+        imp_df = do_imputation(_get_fh("s3+input://es-algo-poc/enrichment_output.csv"))
+    except Algorithmia.errors.AlgorithmException as exc:
+        return {
+            "success": False,
+            "error": _get_traceback(exc)
+        }
+    except Exception as exc:
+        return {
+            "success": False,
+            "error": "Unexpected exception {}".format(_get_traceback(exc))
+        }
+
+    output = imp_df.sum().reset_index().to_csv(index=False)
+    Algorithmia.client().file("s3+input://es-algo-poc/imputation_output.csv").put(str(output))
+
+    return {
+            "success": True,
+            "data": output  # .to_json(orient='records')[1:-1].replace('}, {','},{')
+            }
+
+
+def do_imputation(input_df):
+    period = 201809
+    # cwd = os.path.dirname(__file__)
+    # imported_data = pd.read_csv('{}/tests/fixtures/test_data_imputation.csv'.format(cwd), index_col=False)
+    responder_data = check_non_response(input_df, period)
+
+    return responder_data  # .to_json(orient='records')
+
+
+def check_non_response(data, period):
+    # Create a dataframe where the response column value is set as 1 i.e non responders
+    non_response_filter = (data["response_type"] == 1) & (data["period"] == period)
+    filtered_non_responders = data[non_response_filter]
+    # check the length of the dataset - if there are rows then run imputation...
+    response_check = len(filtered_non_responders.index)
+    if response_check > 0:
+        # Ensure that only responder_ids with a response type of 2 (returned) get
+        # picked up
+        data = data[data['response_type'] == 2]
+        # Select and order the columns required for imputation.
+        ordered_data = data[['responder_id', 'land_or_marine', 'region', 'strata', 'Q601_asphalting_sand',
+                             'Q602_building_soft_sand', 'Q603_concreting_sand', 'Q604_bituminous_gravel',
+                             'Q605_concreting_gravel', 'Q606_other_gravel',
+                             'Q607_constructional_fill', 'Q608_total', 'period'
+
+                             ]]
+        return ordered_data
+    else:
+        print('There are no non responders!')
+        print('Skipping Imputation!')
+        return
+
+# if __name__ == "__main__":
+    # apply("z")
